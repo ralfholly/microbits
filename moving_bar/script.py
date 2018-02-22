e6851fa3205621f0e5ccf060521b34bd5f070e83
Ralf Holly <ralf.holly@approxion.com>
2018-02-22 18:36:14 +0100

    Added unittests

--- moving_bar/script.py
+++ moving_bar/script.py
@@ -1,4 +1,4 @@
-from microbit import *
+import microbit as mbit
 
 ROW_COUNT = 5
 BASE_DELAY = 64
@@ -32,28 +32,37 @@ LED_STATES_NO_GLOW = [
 LED_STATES = [LED_STATES_NO_GLOW, LED_STATES_GLOW]
 
 
-if __name__ == '__main__':
+def main():
+    state_params = {'delay':BASE_DELAY, 'led_states_index':0}
     i = 0
-    delay = BASE_DELAY
-    led_states_index = 0
 
     while True:
-        row_str = LED_STATES[led_states_index][i]
-        matrix_str = (row_str + ':') * ROW_COUNT
-        image = Image(matrix_str)
-        display.show(image)
+        state_params['row_str'] = LED_STATES[state_params['led_states_index']][i]
+        step(state_params)
+        mbit.sleep(state_params['delay'])
+        i = (i + 1) % len(LED_STATES[state_params['led_states_index']])
 
-        if button_a.was_pressed():
-            delay = min(1000, delay + 10)
-        elif button_b.was_pressed():
-            delay = max(10, delay - 10)
-        sleep(delay)
 
-        if accelerometer.was_gesture('up'):
-            led_states_index = (led_states_index + 1) % len(LED_STATES)
+def step(state_params):
+    update_display(state_params)
+    process_input(state_params)
 
-        i = (i + 1) % len(LED_STATES[led_states_index])
 
-def foo(a, b):
-    return a + b
+def update_display(state_params):
+    matrix_str = (state_params['row_str'] + ':') * ROW_COUNT
+    image = mbit.Image(matrix_str)
+    mbit.display.show(image)
+
+
+def process_input(state_params):
+    if mbit.button_a.was_pressed():
+        state_params['delay'] = min(1000, state_params['delay'] + 10)
+    elif mbit.button_b.was_pressed():
+        state_params['delay'] = max(10, state_params['delay'] - 10)
+    if mbit.accelerometer.was_gesture('up'):
+        state_params['led_states_index'] = (state_params['led_states_index'] + 1) % len(LED_STATES)
+
+
+if __name__ == '__main__':
+    main()
 
--- moving_bar/tests/test.py
+++ moving_bar/tests/test.py
@@ -2,11 +2,82 @@ import unittest
 from unittest.mock import Mock
 
 # Mock libs used by SUT.
-from mock_microbit import mocked_microbit
+import microbit as mbit
 
 # The SUT.
 import script
 
 class TestScript(unittest.TestCase):
-    def test_do(self):
-        self.assertEqual(3, script.foo(1, 2))
+    state_params = None
+
+    def setUp(self):
+        self.state_params = {'row_str':"", 'delay':0, 'led_states_index':0}
+        mbit.button_a.was_pressed.return_value = False
+        mbit.button_b.was_pressed.return_value = False
+        mbit.accelerometer.was_gesture.return_value = False
+        mbit.Image = Mock()
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_trivial(self):
+        self.assertEqual(3, 2 + 1)
+
+
+    # pylint:disable=invalid-name
+    def test_delay_stays_same_on_no_input(self):
+        current_delay = 42
+        self.state_params['delay'] = current_delay
+        script.step(self.state_params)
+        self.assertEqual(current_delay, self.state_params['delay'])
+        script.step(self.state_params)
+        self.assertEqual(current_delay, self.state_params['delay'])
+        script.step(self.state_params)
+        self.assertEqual(current_delay, self.state_params['delay'])
+
+
+    # pylint:disable=invalid-name
+    def test_delay_up_on_button_a_pressed(self):
+        current_delay = 42
+        self.state_params['delay'] = current_delay
+        mbit.button_a.was_pressed.return_value = True
+        script.step(self.state_params)
+        self.assertEqual(current_delay + 10, self.state_params['delay'])
+
+
+    # pylint:disable=invalid-name
+    def test_delay_down_on_button_b_pressed(self):
+        current_delay = 42
+        self.state_params['delay'] = current_delay
+        mbit.button_b.was_pressed.return_value = True
+        script.step(self.state_params)
+        self.assertEqual(current_delay - 10, self.state_params['delay'])
+
+    # pylint:disable=invalid-name
+    def test_states_index_updated_if_gesture_up(self):
+        self.state_params['led_states_index'] = 0
+        script.step(self.state_params)
+        self.assertEqual(0, self.state_params['led_states_index'])
+        mbit.accelerometer.was_gesture.side_effect = lambda value: True if value == 'up' else False
+        script.step(self.state_params)
+        self.assertEqual(1, self.state_params['led_states_index'])
+        script.step(self.state_params)
+        self.assertEqual(0, self.state_params['led_states_index'])
+
+    # pylint:disable=invalid-name
+    def test_display_updated_correctly(self):
+        test_str = '12345'
+        self.state_params['row_str'] = test_str
+
+        def side_effect(matrix_str):
+            row_strs = matrix_str.split(':')
+            row_strs.pop()
+            self.assertEqual(5, len(row_strs))
+            for row_str in row_strs:
+                self.assertEqual(test_str, row_str)
+            return None
+
+        mbit.Image.side_effect = side_effect
+        script.step(self.state_params)
